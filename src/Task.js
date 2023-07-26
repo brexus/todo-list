6ba22d3dd813fc0eaf26872eb472dbd022a028e5
brexus <92671269+brexus@users.noreply.github.com>
2023-07-26 15:58:03 +0200

    delete redundant whitespaces

--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,10 @@
 
-class Task {
+export default class Task {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
     }
+
 }
-
-
-
